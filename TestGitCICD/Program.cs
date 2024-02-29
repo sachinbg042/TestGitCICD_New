cd7e7f45a71e2f20ce2a83937f2264515db919cb
BG, Sachinkumar <Sachinkumar.BG@smith-nephew.com>
2024-02-29 12:40:06 +0530

    Add project files.

---
+++ TestGitCICD/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    public class Test
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Test Auto Git Repo CICD");
+        Console.ReadLine(); 
+    }
+}
+
